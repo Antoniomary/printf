619e8a23c0025b8d9e0eea6d4fd1af8f804df927
Antoniomary <amanso.totti@yahoo.com>
2023-10-17 06:14:03 -0700

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _printf - a function that produces output according to a format.
+ * @format: is a character string.
+ *
+ * Return: number of characters printed (excluding the null byte
+ * used to end output to strings).
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	Buffer b = {0};
+	int total_count = 0, temp;
+
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			++format;
+			if (*format)
+			{
+				temp = parser(&args, &b, &format);
+				total_count += temp;
+			}
+		}
+		else
+		{
+			b.buf[b.index++] = *format;
+			++total_count;
+			++format;
+		}
+
+		print_buffer(&b);
+	}
+	va_end(args);
+
+	flush_buffer(&b);
+
+	return (total_count);
+}
