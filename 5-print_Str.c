bd944e40e608d4569f92cd62cda71b06dc348451
Antoniomary <amanso.totti@yahoo.com>
2023-10-17 06:15:57 -0700

    5-print_Str.c

---
+++ 5-print_Str.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_Str - a function that handles specifier S.
+ * @args: is a pointer to the argument to be formatted.
+ * @b: is a pointer to a structure for a buffer.
+ * @val: is a structure holding info about the specifiers given by the user.
+ *
+ * Description: Non printable characters (0 < ASCII value < 32 or >= 127) are
+ * printed this way: \x, followed by the ASCII code value in hexadecimal (upper
+ * case - always 2 characters).
+ * Return: the number of characters of the processed formated string.
+ */
+int print_Str(va_list *args, Buffer *b, formatSpecifier val)
+{
+	char *s = va_arg(*args, char *);
+	int count = 0;
+
+	(void) val;
+
+	if (!s)
+		return (null_handler(b));
+
+	for ( ; *s; ++s)
+	{
+		if (*s >= 32 && *s <= 126)
+		{
+			print_buffer(b);
+			b->buf[b->index++] = *s;
+			++count;
+			continue;
+		}
+
+		if (b->index >= BUF_SIZE - 4)
+			flush_buffer(b);
+
+		b->buf[b->index++] = '\\';
+		b->buf[b->index++] = 'x';
+		b->buf[b->index++] = Hex_map(*s / 16u);
+		b->buf[b->index++] = Hex_map(*s % 16u);
+		count += 4;
+	}
+
+	return (count);
+}
