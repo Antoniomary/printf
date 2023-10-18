7239fedc54ee95359a2daba3c45f07a548c2721f
Antoniomary <amanso.totti@yahoo.com>
2023-10-17 17:10:52 -0700

    help_str_func.c

--- help_str_func.c
+++ help_str_func.c
@@ -1,27 +1,6 @@
 #include "main.h"
 
 /**
- * null_handler - a function that writes "(null)" to buffer when a pointer
- * is NULL.
- * @b: the buffer to write to.
- *
- * Return: Always 6 (the length of the string "(null)").
- */
-int null_handler(Buffer *b)
-{
-	char *null = "(null)";
-
-	for ( ; *null; ++null)
-	{
-		print_buffer(b);
-
-		b->buf[b->index++] = *null;
-	}
-
-	return (6);
-}
-
-/**
  * strcpy_to_buffer - a function that copies a string to buffer.
  * @s: pointer to the string to copy.
  * @b: a structure to the buffer.
